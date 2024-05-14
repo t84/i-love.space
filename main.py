98a3d80365234aba3fe5674f56e8bc91413a37c1
Slakoth <69316271+Joshua-3@users.noreply.github.com>
2024-05-14 01:36:21 -0500

    d

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import requests, base64
 from datetime import datetime, timedelta
+from flask_caching import Cache
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 api_key = "28662158-d89a-4a27-a202-0584ee64e9cf:ecf887cc93015e93ba12e7deec48f22837cdab04b352f4a60ce7e7c748c84e92b8ad0120944c8c1a16b28c50b9effbd626e57cdfdf6b74dac368814da98ebdfdac392c47290de1ddfc96ed3fe94e68ef2961c30ba3e292107ca1da2c4e99e4328396a0c2fbb98c2bb5f402c96ef08eb9"
 apikey = "49be3a765aa44ec1a02e6baddcaaeb50"
@@ -9,25 +13,15 @@ app = Flask(__name__)
 DEVELOPMENT_ENV = True
 
 
-RATE_LIMIT_DURATION = timedelta(seconds=10)
+# Configure Flask-Caching
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-# Define endpoints where you want to apply rate limiting
-api = ['/api/update_iss']
-
-# Store the last access time for each endpoint and IP address
-last_access = {}
-
-@app.before_request
-def rate_limit():
-    endpoint = request.endpoint
-    user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    remote_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-
-    print(remote_ip, user_ip, remote_ip != user_ip)
-
-    if endpoint in api:
-        if remote_ip != user_ip:
-            return jsonify({'error': 'Access forbidden.'}), 403
+# Configure Flask-Limiter
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["10 per minute"]
+)
 
 
 
@@ -68,6 +62,8 @@ def index():
     
 
 @app.route('/api/update_iss', methods=['GET'])
+@limiter.limit("1 per 10 seconds")
+@cache.cached(timeout=10)  
 def update_iss():
     r = requests.get('https://api.wheretheiss.at/v1/satellites/25544').json()
 
