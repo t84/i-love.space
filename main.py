d510841da0f6251cf72c5d1846aad82cbb316170
Slakoth <69316271+Joshua-3@users.noreply.github.com>
2024-05-14 01:03:34 -0500

    d

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests, base64
 from datetime import datetime
 
 api_key = "28662158-d89a-4a27-a202-0584ee64e9cf:ecf887cc93015e93ba12e7deec48f22837cdab04b352f4a60ce7e7c748c84e92b8ad0120944c8c1a16b28c50b9effbd626e57cdfdf6b74dac368814da98ebdfdac392c47290de1ddfc96ed3fe94e68ef2961c30ba3e292107ca1da2c4e99e4328396a0c2fbb98c2bb5f402c96ef08eb9"
-
+apikey = "49be3a765aa44ec1a02e6baddcaaeb50"
 app = Flask(__name__)
 
 DEVELOPMENT_ENV = True
@@ -14,52 +14,9 @@ def index():
 
     try:
         data = requests.get(f"http://ip-api.com/json/{user_ip}", timeout=5).json()
-    #    headers = {
-    #        'Content-Type': 'application/json',
-    #        'Authorization': f'Basic {base64.b64encode(api_key.encode()).decode()}',
-    #    }
-    #    
-    #    # Moon Phase Image Request
-    #    moon_json_data = {
-    #        "format": "png",
-    #        "style": {
-    #            "moonStyle": "sketch",
-    #            "backgroundStyle": "stars",
-    #            "backgroundColor": "red",
-    #            "headingColor": "white",
-    #            "textColor": "red"
-    #        },
-    #        "observer": {
-    #            "latitude": data['lat'],
-    #            "longitude": data['lon'],
-    #            "date": datetime.now().strftime("%Y-%m-%d")
-    #        },
-    #        "view": {
-    #            "type": "portrait-simple"
-    #        }
-    #    }
-    #    moon_response = requests.post('https://api.astronomyapi.com/api/v2/studio/moon-phase', headers=headers, json=moon_json_data, timeout=10)
-    #    moon_response.raise_for_status()  # Raise an exception for non-200 status codes
-    #    moon_image_url = moon_response.json()['data']['imageUrl']
-    #    
-    #    # Stars Image Request
-    #    stars_json_data = {
-    #        "style": "inverted",
-    #        "observer": {
-    #            "latitude": data['lat'],
-    #            "longitude": data['lon'],
-    #            "date": datetime.now().strftime("%Y-%m-%d")
-    #        },
-    #        "view": {
-    #            "type": "constellation",
-    #            "parameters": {
-    #                "constellation": "and"
-    #            }
-    #        }
-    #    }
-    #    stars_response = requests.post('https://api.astronomyapi.com/api/v2/studio/star-chart', headers=headers, json=stars_json_data, timeout=10)
-    #    stars_response.raise_for_status()  # Raise an exception for non-200 status codes
-    #    stars_image_url = stars_response.json()['data']['imageUrl']
+        
+
+
         r = requests.get('https://api.wheretheiss.at/v1/satellites/25544').json()
 
         headers = {
@@ -67,19 +24,42 @@ def index():
             'Referer': 'https://i-love.space',  
         }
 
-        print(r['latitude'], r['longitude'])
+        #print(r['latitude'], r['longitude'])
 
-        r2 = requests.get(f"https://nominatim.openstreetmap.org/reverse?lat=${float(r['latitude'])}&lon=${float(r['longitude'])}&format=json", headers=headers).json()
-        print(r2)
+        url = f"https://api.geoapify.com/v1/geocode/reverse?lat={r['latitude']}&lon={r['longitude']}&apiKey={apikey}"
+
+        # Make the request to the geocoding service
+        r2 = requests.get(url, headers=headers).json()        
+        #print(r2)
+        country_name = r2['features'][0]['properties']['name']
         
-        return render_template("index.html", json=r, ip=user_ip)
+        iss_json = {"data": {"coordinates": {"latitude": r['latitude'], "longitude": r['longitude']}, "country_name": country_name}}
+
+
+        return render_template("index.html", country=iss_json['data']['country_name'], lat=iss_json['data']["coordinates"]['latitude'], lon=iss_json['data']["coordinates"]['longitude'], ip=user_ip)
     
     except requests.RequestException as e:
-        # Log the error
         print(f"Error fetching data: {e}")
-        # Return an error page or handle the error as appropriate
         return "An error occurred while fetching data. Please try again later."
+    
+@app.route('/api/update_iss', methods=['GET'])
+def update_iss():
+    r = requests.get('https://api.wheretheiss.at/v1/satellites/25544').json()
 
+    headers = {
+        'User-Agent': 'https://i-love.space',
+        'Referer': 'https://i-love.space',  
+    }
+
+    #print(r['latitude'], r['longitude'])
+
+    url = f"https://api.geoapify.com/v1/geocode/reverse?lat={r['latitude']}&lon={r['longitude']}&apiKey={apikey}"
+
+    r2 = requests.get(url, headers=headers).json()        
+    #print(r2)
+    country_name = r2['features'][0]['properties']['name']
+
+    return jsonify({"data": {"coordinates": {"latitude": r['latitude'], "longitude": r['longitude']}, "country_name": country_name}})
 
 if __name__ == "__main__":
     app.run(debug=DEVELOPMENT_ENV, port=5000, threaded=True)
