39949e694edd3126a9732806d5b9d0fb0ba53552
Slakoth <69316271+Joshua-3@users.noreply.github.com>
2024-05-18 21:13:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def apod():
                 cache.set("apod", data, timeout=3600)
 
                 return jsonify({"message": "success", "data": data})
-            elif r.status_code == 404 and "No APOD data available for date" in r.json().get('msg', ''):
+            elif "No APOD data available for date" in r.json().get('msg', ''):
                 yesterday = datetime.now() - timedelta(days=1)  
                 yesterday_date = yesterday.date().isoformat()
                 r_yesterday = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={apikey_nasa}&date={yesterday_date}")
